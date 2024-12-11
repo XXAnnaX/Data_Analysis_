a9dcef9d22687285c60cf3d1352177df19e33c3c
XXAnnaX <lozovaanna45@gmail.com>
2024-12-11 20:36:12 +0000

    result

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,44 @@
 import numpy as np
 
 def calculate(list):
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
+    
+    matrix = np.array(list).reshape(3, 3)
+    
+    # Обчислення середнього
+    mean_axis1 = np.mean(matrix, axis=0).tolist()
+    mean_axis2 = np.mean(matrix, axis=1).tolist()
+    mean_flattened = np.mean(matrix).tolist()
 
-
-
+    # Формування словника
+    calculations = {
+        'mean': [mean_axis1, mean_axis2, mean_flattened],
+        'variance': [
+            np.var(matrix, axis=0).tolist(),
+            np.var(matrix, axis=1).tolist(),
+            np.var(matrix).tolist()
+        ],
+        'standard deviation': [
+            np.std(matrix, axis=0).tolist(),
+            np.std(matrix, axis=1).tolist(),
+            np.std(matrix).tolist()
+        ],
+        'max': [
+            np.max(matrix, axis=0).tolist(),
+            np.max(matrix, axis=1).tolist(),
+            np.max(matrix).tolist()
+        ],
+        'min': [
+            np.min(matrix, axis=0).tolist(),
+            np.min(matrix, axis=1).tolist(),
+            np.min(matrix).tolist()
+        ],
+        'sum': [
+            np.sum(matrix, axis=0).tolist(),
+            np.sum(matrix, axis=1).tolist(),
+            np.sum(matrix).tolist()
+        ]
+    }
 
     return calculations
